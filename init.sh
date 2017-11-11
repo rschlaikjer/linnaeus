f3b4e5db2294cb4cc88e6e2a0799958cf4c8139f
mbc <malcolmbalch@gmail.com>
2017-11-11 13:25:04 -0500

    Config for using caffe python bindings in a different directory

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+
+CAFFE_ROOT="/home/malcolm/Projects/caffe"
+export PYTHONPATH="$CAFFE_ROOT/distribute/python:$PYTHONPATH"
+export LD_LIBRARY_PATH="$CAFFE_ROOT/distribute/lib/"
+python testing.py
